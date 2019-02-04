dbca210f624edd30c09a51ed2cb8e0e735ef590b
Obie <oborms@icloud.com>
2019-02-04 08:47:12 -0800

    020

---
+++ ObadiahBorms02.sh
@@ -0,0 +1,6 @@
+git config --list;
+git config --global user.username ObadiahBoms02;
+git config --global user.name  Obie;
+git config --global user.user ObadiahBorms02;
+git config --global user.email oborms@icloud.com;
+git config --list;
